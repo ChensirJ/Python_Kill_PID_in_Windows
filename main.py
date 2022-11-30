0bb239df95182308b7bff16447be6ecf9497f55a
jchen-objt <joey.chen@aptean.com>
2022-11-30 20:41:04 +0800

    Add UI

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import os
 from netstat import *
-from sys import argv
+from tkinter import *
+from tkinter.messagebox import *
 
 temp_netstat_list = []
 netstat_object_list = []
 pid_set = set()
 
+win_width = 400
+win_height = 150
+title = "Kill Process"
 
-def main(port):
-    netstat_list = os.popen("netstat -ano | findstr \"" + port + "\"").read().split(" ")
+root = Tk()
+root.title(title)
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = int((screen_width - win_width) / 2)
+y = int((screen_height - win_height) / 2)
+
+root.geometry("%sx%s+%s+%s" % (win_width, win_height, x, y))
+
+root.resizable(False, False)
+
+frame = Frame(root)
+frame.place(rely=.5, relx=.5, x=-80, y=-40, width=245, height=100)
+
+Label(frame, text="Port").grid(row=0)
+
+port_var = StringVar()
+Entry(frame, textvariable=port_var).grid(row=0, column=1, columnspan=2, padx=10)
+
+
+def kill_process():
+    if port_var.get() == "" or port_var.get() is None:
+        showerror("Error", "Please input port number!")
+        return
+
+    netstat_list = os.popen("netstat -ano | findstr \"" + port_var.get() + "\"").read().split(" ")
 
     for i in netstat_list:
         if '\n' in i:
@@ -21,9 +51,14 @@ def main(port):
     for i in netstat_object_list:
         print(i)
 
-    msg_list = os.popen("taskkill /f /pid " + pid_set.pop()).read().split(" ")
-    print(msg_list)
+    showinfo("Info", os.popen("taskkill /f /pid " + pid_set.pop()).read())
 
 
-if __name__ == '__main__':
-    main(argv[1])
+def reset():
+    port_var.set("")
+
+
+Button(frame, text="Confirm", command=kill_process, width=8).grid(row=1, column=1, pady=8)
+Button(frame, text="Reset", command=reset, width=8).grid(row=1, column=2, padx=2, pady=8)
+
+root.mainloop()
